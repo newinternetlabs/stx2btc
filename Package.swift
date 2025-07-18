5a917bb917224abccb52370355bb7866645183d4
Larry Salibra <larry@newinternetlabs.com>
2025-07-19 07:20:16 +0800

    Update Package.swift for release v0.3.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "stx2btcFFI",
-            path: "target/xcframework/stx2btc.xcframework"
+            url: "https://github.com/newinternetlabs/stx2btc/releases/download/v0.3.1/stx2btc.xcframework.zip",
+            checksum: "5337bbca90b68698a33fca6647aa42f8557781f60404bc14401a0dbed6298c00"
         ),
         .target(
             name: "stx2btc",
